c601b15af8ea162c00cb094c9968b9327174a2f5
arracso <arracso@gmail.com>
2018-06-28 11:05:26 +0200

    Updated expand item

--- app/src/main/java/edu/udg/exit/heartrate/Components/ExpandItem.java
+++ app/src/main/java/edu/udg/exit/heartrate/Components/ExpandItem.java
@@ -2,7 +2,9 @@ package edu.udg.exit.heartrate.Components;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
 import edu.udg.exit.heartrate.R;
 
@@ -10,12 +12,15 @@ public class ExpandItem extends LinearLayout {
 
     public ExpandItem(Context context) {
         super(context);
+        initializeViews(context);
     }
     public ExpandItem(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initializeViews(context);
     }
     public ExpandItem(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        initializeViews(context);
     }
 
     /**
@@ -31,6 +36,14 @@ public class ExpandItem extends LinearLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
 
+        LinearLayout hiddenComponent = (LinearLayout) findViewById(R.id.hidden_component);
+        int index = indexOfChild(hiddenComponent);
+
+        while(index+1 < getChildCount()){
+            View child = getChildAt(index+1);
+            removeViewAt(index+1);
+            hiddenComponent.addView(child);
+        }
     }
 
 }
