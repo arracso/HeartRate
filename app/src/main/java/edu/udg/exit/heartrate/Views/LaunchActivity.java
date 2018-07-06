4e8242fdb21ccc8979819892fdab68ba3fdad339
arracso <arracso@gmail.com>
2018-07-06 12:51:39 +0200

    Use refresh token to refresh access token

--- app/src/main/java/edu/udg/exit/heartrate/Views/LaunchActivity.java
+++ app/src/main/java/edu/udg/exit/heartrate/Views/LaunchActivity.java
@@ -103,7 +103,6 @@ public class LaunchActivity extends Activity {
                     }
                 },DELAY);
             }
-            startMainActivity();
             return;
         }
 
